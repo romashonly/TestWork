d23db1cfab8e40f478674446aadca68d13603cb1
Roman <roma.shurkin@mail.ru>
2019-12-23 14:02:14 +0300

    done

--- TestTask.playground/Contents.swift
+++ TestTask.playground/Contents.swift
@@ -1,35 +1,52 @@
 import UIKit
 
+enum Check {
+    case startFound
+    case endFound
+    case state
+}
+
 extension String {
     func getRange(positon: Int) -> (Int, Int) {
     
         var indexOfStart: Int = -1
         var indexOfEnd: Int = -1
-        var flag: Bool = true
+        var flag: Check = Check.state
         
         for (i, v) in self.enumerated() {
+            
             if (i == positon) {
-                flag = false
+                flag = Check.startFound
             }
-            if v == "\n" && flag {
-                indexOfStart = i
-            }
-            else if v == "\n" && !flag {
-                indexOfEnd = i
+            
+            if v == "\n" {
+                print(1)
+                print(flag)
+                switch flag {
+                case .startFound:
+                    indexOfEnd = i
+                    flag = Check.endFound
+                case .endFound:
+                    flag = Check.state
+                case .state:
+                    indexOfStart = i
+                }
             }
         }
         
         indexOfStart = indexOfStart == -1 ? 0 : indexOfStart
-        indexOfEnd = indexOfEnd == -1 ? self.count - 1 : indexOfEnd
+        indexOfStart = indexOfStart != 0 ? indexOfStart + 1 : indexOfStart
+        indexOfEnd = indexOfEnd == -1 ? self.count : indexOfEnd
+        indexOfEnd = indexOfEnd != 0 ? indexOfEnd - 1 : indexOfEnd
         
         return (indexOfStart, indexOfEnd)
     }
 }
 
 var str: String = """
-gnwkw
-sdf
-sdvsd
+gnwkwfffff
+sdfgnwkwe
+sdvsdsdvsdsdvsdsdvsd
 """
 
-print(str.getRange(positon: 7))
+print(str.getRange(positon: 5))
